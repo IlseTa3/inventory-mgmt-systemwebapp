0a6182e6d888f68931074e3f8a7f15938d444974
Ilse Tastenhoye <ilse.tastenhoye@gmail.com>
2024-09-28 15:55:05 +0200

    Added entities + migration with db

--- InventoryManagementSystem/Program.cs
+++ InventoryManagementSystem/Program.cs
@@ -1,7 +1,15 @@
+using InventoryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure MySQL connection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    new MySqlServerVersion(new Version(8, 0, 36))));
 
 var app = builder.Build();
 
@@ -9,15 +17,12 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
